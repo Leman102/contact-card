c39aaf8bea225e678b576c5d9056c295374999c5
Leoman <leo.pinzon102@gmail.com>
2022-09-17 17:08:05 -0400

    adding Get and Post

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,3 +17,46 @@ export const initDb = async () => {
         }
     })
 }
+
+// Exported READ function
+export const getDb = async () => {
+    console.log('GET from database');
+
+    //create a connection to the db and version we want to use
+    const contactDb = await openDB('contact_db',1);
+
+    //Create a new transaction and specify the store and data privileges
+    const tx = contactDb.transaction('contacts','readonly');
+
+    //open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    //use the .getAll() method to get all data in db
+    const request = store.getAll();
+
+    //Get request confirmation
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+};
+
+// Exported POST function
+export const postDb = async (name, email, phone, profile) => {
+    console.log('POST to the database');
+
+    //create a connection to the db and version we want to use
+    const contactDb = await openDB('contact_db',1);
+
+    //Create a new transaction and specify the store and data privileges
+    const tx = contactDb.transaction('contacts','readwrite');
+
+    //open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    //use the .add() method on the store and pass in the content
+    const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+    
+    //Get request confirmation
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+};
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,11 @@
-import "./form";
-import "./submit";
+import { toggleForm, clearForm } from "./form";
+// import "./submit";
 
 //import database
-import { initDb } from './database';
+import { initDb, getDb, postDb } from './database';
+
+//import fetch card
+import { fetchCards } from "./card";
 
 //import css 
 import "../css/index.css";
@@ -19,8 +22,48 @@ import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
     initDb();
+    fetchCards();
+    // // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+    // getDb();
+    // postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+    // getDb();
+
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
 });
 
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+    toggleForm()
+})
+
+form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+    if (submitBtnToUpdate == false) {
+        postDb(name, email, phone, profile);
+    } else {
+        fetchCards();
+        // Toggles the submit button back to POST functionality
+        submitBtnToUpdate = false;
+    }
+
+    // Clear form
+    clearForm();
+    // Toggle form
+    toggleForm();
+    // Reload the DOM
+    fetchCards();
+});
